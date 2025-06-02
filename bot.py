e0ec53e85c0a63fa8309ef62977df83c3574fb4e
sharko-rgb <72403836+sharko-rgb@users.noreply.github.com>
2025-06-03 00:51:54 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
 from aiogram import Bot, Dispatcher, F
-from aiogram.filters import Command, Text
+from aiogram.filters import Command
+from aiogram.filters.callback_query import Text
 from aiogram.types import Message, CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.storage.memory import MemoryStorage
 import asyncio
+
 
 API_TOKEN = "7853853505:AAEhTPDeWUlX67naGu5JhW9-maep1yesUD0"
 ADMIN_ID = 1346038165  # твой ID
